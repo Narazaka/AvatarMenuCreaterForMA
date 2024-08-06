3948d4dca4442a20a0ccb63d1d6db1df83ca0e9d
Narazaka <info@narazaka.net>
2024-08-06 11:31:49 +0900

    whitelist

--- Core/AvatarToggleMenu.cs
+++ Core/AvatarToggleMenu.cs
@@ -258,7 +258,7 @@ namespace net.narazaka.avatarmenucreator
                 var gameObjectRef = GetGameObject(child);
                 var names = gameObjectRef == null ? ToggleBlendShapes.Names(child).ToList() : Util.GetBlendShapeNames(gameObjectRef);
                 var parameters = gameObjectRef == null ? ToggleShaderParameters.Names(child).ToFakeShaderParameters().ToList() : ShaderParametersCache.GetFilteredShaderParameters(gameObjectRef);
-                var components = gameObjectRef == null ? ToggleValues.Names(child).Select(n => n.Type).ToList() : gameObjectRef.GetAllComponents().Select(c => TypeUtil.GetType(c)).ToList();
+                var components = gameObjectRef == null ? ToggleValues.Names(child).Select(n => n.Type).ToList() : gameObjectRef.GetAllComponents().Select(c => TypeUtil.GetType(c)).FilterByVRCWhitelist().ToList();
                 var properties = components.SelectMany(c => c.GetAvailableMembers()).ToList();
 
                 var materials = allMaterials[child];
